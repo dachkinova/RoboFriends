70815f46087172932b4891c408cd0d279753fd16
Angelina Dachkinova <56481374+dachkinova@users.noreply.github.com>
2022-01-21 16:07:56 +0200

    fixing security issues updated libraries

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+<link href="//db.onlinewebfonts.com/c/b593a614952261da06c53a02d1954f44?family=SEGA+LOGO+FONT" rel="stylesheet" type="text/css" />
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users });
+            })
+
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+
+    }
+    render() {
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return !robots.length ?
+            <h1>Loading...</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+    }
+}
+
+export default App;
